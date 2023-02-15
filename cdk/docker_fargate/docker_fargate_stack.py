9ee1e76ed89c0667d509ae4d3700cafcb0755a64
Khai Do <zaro0508@gmail.com>
2023-02-15 10:17:30 -0800

    fix linter errors

--- cdk/docker_fargate/docker_fargate_stack.py
+++ cdk/docker_fargate/docker_fargate_stack.py
@@ -1,12 +1,12 @@
 from aws_cdk import (Stack,
-	aws_ec2 as ec2, 
-	aws_ecs as ecs,
-	aws_ecs_patterns as ecs_patterns,
-	aws_ssm as ssm,
-	aws_elasticloadbalancingv2 as elbv2,
-	aws_route53 as r53,
-	Duration,
-	Tags)
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_ssm as ssm,
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_route53 as r53,
+    Duration,
+    Tags)
 
 import os
 import aws_cdk.aws_secretsmanager as sm
@@ -72,13 +72,13 @@ def create_secret(scope: Construct, name: str) -> str:
     # see also: ecs.Secret.from_ssm_parameter(ssm.IParameter(parameter_name=name))
 
 def get_hosted_zone_name() -> str:
-	return os.getenv(HOSTED_ZONE_NAME)
-	
+    return os.getenv(HOSTED_ZONE_NAME)
+
 def get_hosted_zone_id() -> str:
-	return os.getenv(HOSTED_ZONE_ID)
-	
+    return os.getenv(HOSTED_ZONE_ID)
+
 def get_host_name() -> str:
-	return os.getenv(HOST_NAME)
+    return os.getenv(HOST_NAME)
 
 class DockerFargateStack(Stack):
 
@@ -91,7 +91,7 @@ class DockerFargateStack(Stack):
         cluster = ecs.Cluster(self, get_cluster_name(), vpc=vpc, container_insights=True)
 
         secrets = {
-        	SECRETS_MANAGER_ENV_NAME: create_secret(self, get_secret_name())
+            SECRETS_MANAGER_ENV_NAME: create_secret(self, get_secret_name())
         }
 
         env_vars = {}
@@ -100,14 +100,14 @@ class DockerFargateStack(Stack):
             env_vars[ENV_NAME]=container_env
 
         task_image_options = ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-    	    	   image=ecs.ContainerImage.from_registry(get_docker_image_name()),
-    	    	   environment=env_vars,
-    	    	   secrets = secrets,
-    	    	   container_port = get_port())
-        
+                   image=ecs.ContainerImage.from_registry(get_docker_image_name()),
+                   environment=env_vars,
+                   secrets = secrets,
+                   container_port = get_port())
+
         zone = r53.PublicHostedZone.from_public_hosted_zone_attributes(self, id=stack_id+"_zone", hosted_zone_id=get_hosted_zone_id(), zone_name=get_hosted_zone_name())
 
- 
+
         #
         # for options to pass to ApplicationLoadBalancedTaskImageOptions see:
         # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_ecs_patterns/ApplicationLoadBalancedTaskImageOptions.html#aws_cdk.aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions
@@ -124,12 +124,12 @@ class DockerFargateStack(Stack):
             protocol=elbv2.ApplicationProtocol.HTTPS,
             ssl_policy=elbv2.SslPolicy.FORWARD_SECRECY_TLS12_RES, # Strong forward secrecy ciphers and TLS1.2 only.
             domain_name=get_host_name(), # The domain name for the service, e.g. “api.example.com.”
-            domain_zone=zone) #  The Route53 hosted zone for the domain, e.g. “example.com.”            
-            
+            domain_zone=zone) #  The Route53 hosted zone for the domain, e.g. “example.com.”
+
         # Overriding health check timeout helps with sluggishly responding app's (e.g. Shiny)
         # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_elasticloadbalancingv2/ApplicationTargetGroup.html#aws_cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup
         # The number of consecutive health check failures required before considering a target unhealthy. For Application Load Balancers, the default is 2.
-        #     
+        #
         load_balanced_fargate_service.target_group.configure_health_check(interval=Duration.seconds(120), timeout=Duration.seconds(60), path="/v1/ui/", healthy_http_codes="200-308", unhealthy_threshold_count=5)
 
         if True: # enable/disable autoscaling
@@ -141,12 +141,12 @@ class DockerFargateStack(Stack):
             # Add more capacity when CPU utilization reaches 50%
             # target_utilization_percent: The target value for CPU utilization across all tasks in the service.
             scalable_target.scale_on_cpu_utilization("CpuScaling",
-            	target_utilization_percent=50
+                target_utilization_percent=50
             )
 
             # Add more capacity when memory utilization reaches 50%
             scalable_target.scale_on_memory_utilization("MemoryScaling",
-            	target_utilization_percent=50
+                target_utilization_percent=50
             )
 
             # Other metrics to drive scaling are discussed here:
